f86a841cbc9d74fa7aa643bb402efdc837a5f85a
Evan Wildenhain <mew2@princeton.edu>
2019-05-08 12:02:00 -0400

    use kaleidoscope texture for cylinder walls as well

--- js/objects.js
+++ js/objects.js
@@ -1,6 +1,52 @@
 const CYLINDER_RADIUS = 1;
 const CYLINDER_HEIGHT = 20;
 
+function NewPieCylinder() {
+    const group = new THREE.Group();
+    const sliceAngle = 2 * Math.PI / 12;
+    for (let i = 0; i < 12; ++i) {
+        const geometry = new THREE.CylinderGeometry(
+            CYLINDER_RADIUS, // radiusTop
+            CYLINDER_RADIUS, // radiusBottom
+            CYLINDER_HEIGHT, // height
+            32, // radialSegments
+            1, // heightSegments
+            true, // openEnded
+            0,
+            2 * Math.PI / 12,
+        );
+        const material = KaleidoscopeMaterial();
+        const cylinder = new THREE.Mesh(geometry, material);
+        material.side = THREE.BackSide; // DoubleSide, FrontSide or BackSide
+        cylinder.rotation.x += Math.PI / 2;
+        cylinder.rotation.y += sliceAngle * i;
+        group.add(cylinder);
+    }
+
+    // add wall lines
+    const numWallLines = 12;
+    const lineRadius = 0.01;
+    for (let i = 0; i < numWallLines; ++i) {
+        const lineGeo = new THREE.CylinderGeometry(
+            lineRadius,
+            lineRadius,
+            CYLINDER_HEIGHT,
+            8,
+            1,
+            true
+        );
+        const lineMat = new THREE.MeshStandardMaterial({ color: 0x222222, metalness: 1.0 });
+        lineMat.side = THREE.FrontSide;
+        const lineCylinder = new THREE.Mesh(lineGeo, lineMat);
+        lineCylinder.rotation.x += Math.PI / 2;
+        const theta = 2 * Math.PI * i / 12;
+        lineCylinder.position.x = 0.99 * CYLINDER_RADIUS * Math.cos(theta);
+        lineCylinder.position.y = 0.99 * CYLINDER_RADIUS * Math.sin(theta);
+        group.add(lineCylinder);
+    }
+    return group;
+}
+
 function NewEnclosingCylinder() {
     const geometry = new THREE.CylinderGeometry(
         CYLINDER_RADIUS, // radiusTop
@@ -10,9 +56,10 @@ function NewEnclosingCylinder() {
         1, // heightSegments
         true // openEnded
     );
-    const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.8 });
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0 });
     const cylinder = new THREE.Mesh(geometry, material);
-    material.side = THREE.DoubleSide; // or FrontSide or BackSide
+    material.side = THREE.BackSide; // DoubleSide, FrontSide or BackSide
+
     cylinder.rotation.x += Math.PI / 2;
     const group = new THREE.Group();
     group.add(cylinder);
@@ -65,13 +112,23 @@ const KaleidoscopeTexture = () => {
     );
     return texture;
 }
+const SingletonKaleidoscopeTexture = KaleidoscopeTexture();
 
 const KaleidoscopeMaterial = () => {
-    const mat = new THREE.MeshStandardMaterial({ color: 0xFFFFFF, metalness: 0.5, map: KaleidoscopeTexture() });
+    const mat = new THREE.MeshStandardMaterial({
+        color: 0xFFFFFF, metalness: 0.0, map: SingletonKaleidoscopeTexture,
+        //  transparent: true, opacity: 0.75
+    });
     return mat;
 }
 
-const SingletonMaterial = KaleidoscopeMaterial();
+const EnclosingKaleidoscopeMaterial = () => {
+    const mat = new THREE.MeshStandardMaterial({
+        color: 0xFFFFFF, metalness: 0.1, map: SingletonKaleidoscopeTexture,
+    });
+    return mat;
+}
+
 
 const TOTAL_NUM_SLICES = 12;
 const SLICE_ANGLE = 2 * Math.PI / TOTAL_NUM_SLICES;
@@ -83,13 +140,13 @@ function NewPieBarrier(
     numSlices, // how many 1/6-th slices are in the barrier
     gapPosition, // the angle (in radians) that the centerline of the gap in the barrier should be at
 ) {
-    const barrierRadius = CYLINDER_RADIUS * 0.99;
+    const barrierRadius = CYLINDER_RADIUS;
     const slices = [];
     const sliceAngle = 2 * Math.PI / TOTAL_NUM_SLICES;
 
     for (let i = 0; i < numSlices; ++i) {
         const geometry = new THREE.CylinderGeometry(barrierRadius, barrierRadius, 1, 32, 1, false, 0, sliceAngle);
-        const material = SingletonMaterial;
+        const material = KaleidoscopeMaterial();
         const cylinder = new THREE.Mesh(geometry, material);
 
         // this is where the kaleidoscope effect comes in!
@@ -110,7 +167,7 @@ function NewPieBarrier(
 
 function NewPlayer() {
     const geometry = new THREE.SphereGeometry(0.2, 12, 12);
-    const material = new THREE.MeshStandardMaterial({ color: 0xFF0000, metalness: 0.5 });
+    const material = new THREE.MeshStandardMaterial({ color: 0xFF0000, metalness: 0.33 });
     const player = new THREE.Mesh(geometry, material);
     return player;
 }
